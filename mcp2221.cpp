3d3fd564866125154b4188ebb3cb0e08a9601818
Samuel Lourenço <16304376+samuelfmlourenco@users.noreply.github.com>
2026-03-08 01:35:33 +0000

    Add files via upload

---
+++ mcp2221.cpp
@@ -0,0 +1,140 @@
+/* MCP2221 class for Qt - Version 1.0.0
+   Copyright (c) 2026 Samuel Lourenço
+
+   This library is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or (at your
+   option) any later version.
+
+   This library is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+   License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with this library.  If not, see <https://www.gnu.org/licenses/>.
+
+
+   Please feel free to contact me via e-mail: samuel.fmlourenco@gmail.com */
+
+
+// Includes
+#include <QObject>
+#include "mcp2221.h"
+extern "C" {
+#include "libusb-extra.h"
+}
+
+MCP2221::MCP2221() :
+    context_(nullptr),
+    handle_(nullptr),
+    disconnected_(false),
+    kernelWasAttached_(false)
+{
+}
+
+MCP2221::~MCP2221()
+{
+    close();  // The destructor is used to close the device, and this is essential so the device can be freed when the parent object is destroyed
+}
+
+// Diagnostic function used to verify if the device has been disconnected
+bool MCP2221::disconnected() const
+{
+    return disconnected_;  // Returns true if the device has been disconnected, or false otherwise
+}
+
+// Checks if the device is open
+bool MCP2221::isOpen() const
+{
+    return handle_ != nullptr;  // Returns true if the device is open, or false otherwise
+}
+
+// Closes the device safely, if open
+void MCP2221::close()
+{
+    if (isOpen()) {  // This condition avoids a segmentation fault if the calling algorithm tries, for some reason, to close the same device twice (e.g., if the device is already closed when the destructor is called)
+        libusb_release_interface(handle_, 0);  // Release the interface
+        if (kernelWasAttached_) {  // If a kernel driver was attached to the interface before
+            libusb_attach_kernel_driver(handle_, 0);  // Reattach the kernel driver
+        }
+        libusb_close(handle_);  // Close the device
+        libusb_exit(context_);  // Deinitialize libusb
+        handle_ = nullptr;  // Required to mark the device as closed
+    }
+}
+
+// Opens the device having the given VID, PID and, optionally, the given serial number, and assigns its handle
+int MCP2221::open(quint16 vid, quint16 pid, const QString &serial)
+{
+    int retval;
+    if (isOpen()) {  // Just in case the calling algorithm tries to open a device that was already sucessfully open, or tries to open different devices concurrently, all while using (or referencing to) the same object
+        retval = SUCCESS;
+    } else if (libusb_init(&context_) != 0) {  // Initialize libusb. In case of failure
+        retval = ERROR_INIT;
+    } else {  // If libusb is initialized
+        if (serial.isNull()) {  // Note that serial, by omission, is a null QString
+            handle_ = libusb_open_device_with_vid_pid(context_, vid, pid);  // If no serial number is specified, this will open the first device found with matching VID and PID
+        } else {
+            handle_ = libusb_open_device_with_vid_pid_serial(context_, vid, pid, reinterpret_cast<unsigned char *>(serial.toLatin1().data()));
+        }
+        if (handle_ == nullptr) {  // If the previous operation fails to get a device handle
+            libusb_exit(context_);  // Deinitialize libusb
+            retval = ERROR_NOT_FOUND;
+        } else {  // If the device is successfully opened and a handle obtained
+            if (libusb_kernel_driver_active(handle_, 0) == 1) {  // If a kernel driver is active on the interface
+                libusb_detach_kernel_driver(handle_, 0);  // Detach the kernel driver
+                kernelWasAttached_ = true;  // Flag that the kernel driver was attached
+            } else {
+                kernelWasAttached_ = false;  // The kernel driver was not attached
+            }
+            if (libusb_claim_interface(handle_, 0) != 0) {  // Claim the interface. In case of failure
+                if (kernelWasAttached_) {  // If a kernel driver was attached to the interface before
+                    libusb_attach_kernel_driver(handle_, 0);  // Reattach the kernel driver
+                }
+                libusb_close(handle_);  // Close the device
+                libusb_exit(context_);  // Deinitialize libusb
+                handle_ = nullptr;  // Required to mark the device as closed
+                retval = ERROR_BUSY;
+            } else {
+                disconnected_ = false;  // Note that this flag is never assumed to be true for a device that was never opened - See constructor for details!
+                retval = SUCCESS;
+            }
+        }
+    }
+    return retval;
+}
+
+// Helper function to list devices
+QStringList MCP2221::listDevices(quint16 vid, quint16 pid, int &errcnt, QString &errstr)
+{
+    QStringList devices;
+    libusb_context *context;
+    if (libusb_init(&context) != 0) {  // Initialize libusb. In case of failure
+        ++errcnt;
+        errstr += QObject::tr("Could not initialize libusb.\n");
+    } else {  // If libusb is initialized
+        libusb_device **devs;
+        ssize_t devlist = libusb_get_device_list(context, &devs);  // Get a device list
+        if (devlist < 0) {  // If the previous operation fails to get a device list
+            ++errcnt;
+            errstr += QObject::tr("Failed to retrieve a list of devices.\n");
+        } else {
+            for (ssize_t i = 0; i < devlist; ++i) {  // Run through all listed devices
+                libusb_device_descriptor desc;
+                if (libusb_get_device_descriptor(devs[i], &desc) == 0 && desc.idVendor == vid && desc.idProduct == pid) {  // If the device descriptor is retrieved, and both VID and PID correspond to the respective given values
+                    libusb_device_handle *handle;
+                    if (libusb_open(devs[i], &handle) == 0) {  // Open the listed device. If successfull
+                        unsigned char str_desc[256];
+                        libusb_get_string_descriptor_ascii(handle, desc.iSerialNumber, str_desc, static_cast<int>(sizeof(str_desc)));  // Get the serial number string in ASCII format
+                        devices += reinterpret_cast<char *>(str_desc);  // Append the serial number string to the list
+                        libusb_close(handle);  // Close the device
+                    }
+                }
+            }
+            libusb_free_device_list(devs, 1);  // Free device list
+        }
+        libusb_exit(context);  // Deinitialize libusb
+    }
+    return devices;
+}
---
+++ mcp2221.h
@@ -0,0 +1,57 @@
+/* MCP2221 class for Qt - Version 1.0.0
+   Copyright (c) 2026 Samuel Lourenço
+
+   This library is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or (at your
+   option) any later version.
+
+   This library is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+   License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with this library.  If not, see <https://www.gnu.org/licenses/>.
+
+
+   Please feel free to contact me via e-mail: samuel.fmlourenco@gmail.com */
+
+
+#ifndef MCP2221_H
+#define MCP2221_H 
+
+// Includes
+#include <QString>
+#include <QStringList>
+#include <libusb-1.0/libusb.h>
+
+class MCP2221
+{
+private:
+    libusb_context *context_;
+    libusb_device_handle *handle_;
+    bool disconnected_, kernelWasAttached_;
+
+public:
+    // Class definitions
+    static const quint16 VID = 0x04d8;     // Default USB vendor ID
+    static const quint16 PID = 0x00dd;     // Default USB product ID
+    static const int SUCCESS = 0;          // Returned by open() if successful
+    static const int ERROR_INIT = 1;       // Returned by open() in case of a libusb initialization failure
+    static const int ERROR_NOT_FOUND = 2;  // Returned by open() if the device was not found
+    static const int ERROR_BUSY = 3;       // Returned by open() if the device is already in use
+
+    MCP2221();
+    ~MCP2221();
+
+    bool disconnected() const;
+    bool isOpen() const;
+
+    void close();
+    int open(quint16 vid, quint16 pid, const QString &serial = QString());
+
+    static QStringList listDevices(quint16 vid, quint16 pid, int &errcnt, QString &errstr);
+};
+
+#endif  // MCP2221_H
